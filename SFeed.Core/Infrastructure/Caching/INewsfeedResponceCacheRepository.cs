7a2ec5ede1ca0e918ac052052c653ad95236828c
yilmazanil <anilyilmaz86@outlook.com>
2018-04-18 16:13:20 +0300

    Revert "Unknown"
    This reverts commit 04ab95f276a3532d4b425298353c39c2db3c70a3.

--- SFeed.Core/Infrastructure/Caching/INewsfeedResponceCacheRepository.cs
+++ SFeed.Core/Infrastructure/Caching/INewsfeedResponceCacheRepository.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace SFeed.Core.Infrastructure.Caching
 {
-    public interface INewsfeedResponseCacheRepository
+    public interface INewsfeedResponceCacheRepository
     {
         IEnumerable<NewsfeedWallPostModel> GetUserFeed(string userId, int skip, int take);
     }
--- SFeed.Core/Models/Newsfeed/NewsfeedWallPostModel.cs
+++ SFeed.Core/Models/Newsfeed/NewsfeedWallPostModel.cs
@@ -1,10 +1,17 @@
-﻿using SFeed.Core.Models.WallPost;
+﻿using SFeed.Core.Models.Comments;
+using SFeed.Core.Models.Newsfeed;
 using System.Collections.Generic;
 
 namespace SFeed.Core.Models.Caching
 {
-    public class NewsfeedWallPostModel : WallPostDetailedModel
+    public class NewsfeedWallPostModel : WallPostCacheModel
     {
+        public IEnumerable<CommentCacheModel> LatestComments { get; set; }
+
+        public int LikeCount { get; set; }
+
+        public int CommentCount { get; set; }
+
         public List<string> FeedDescription { get; set; }
     }
 }
